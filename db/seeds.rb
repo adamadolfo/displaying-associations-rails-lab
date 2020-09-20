c46fdc95d18d6c1454e8a892824dd7f39cd42475
Adam <adamadolfo8@gmail.com>
2020-09-20 15:49:46 -0400

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+dgd = Artist.create!(name: "dance gavin dance")
+avril = Artist.create!(name: "Avril Lavigne")
+Song.destroy_all
+dgd.songs.create!(title: "carl barker")
+dgd.songs.create!(title: "alex english")
+avril.songs.create!(title: "sk8er boi")
+avril.songs.create!(title: "girlfriend")
+avril.songs.create!(title: "when your gone")
+
